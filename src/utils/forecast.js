074f78b2505796401502c8d831df113f3efaa80e
Tyler Weisner <tylerw909@gmail.com>
2019-06-11 11:49:51 -0400

    updated weather

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -9,7 +9,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback("Unable to find that position.", undefined)
         } else {
-            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out, with a " + body.currently.precipProbability + "% chance of rain.")
+            console.log(body.daily.data[0])
+            callback(undefined, body.daily.data[0].summary + " It is currently " + body.currently.temperature + " degrees out, with a " + body.currently.precipProbability + "% chance of rain. Today there is a low of " + body.daily.data[0].temperatureLow + " and a high of " + body.daily.data[0].temperatureHigh + ".")
         }
     })
 }
